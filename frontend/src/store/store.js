230a6b50343f2c7670de992b012f0af49820fad3
alscotty <alawrencescott@gmail.com>
2020-01-19 16:40:09 -0800

    add configure store and reducer file

---
+++ frontend/src/store/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from '../reducers/root_reducer'
+
+const configureStore = (preloadedState = {}) => {
+    const middlewareEnhancer = applyMiddleware(thunk, logger);
+    const composedEnhancer = composeWithDevTools(middlewareEnhancer);
+
+    return (
+        createStore(
+            rootReducer,
+            preloadedState,
+            composedEnhancer
+        )
+    )
+};
+
+export default configureStore;
