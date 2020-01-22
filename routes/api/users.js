60d2df2b141cd90f4a4fa3ef2f3cce0becc4ad4a
codybreene <codybreene@gmail.com>
2020-01-21 21:51:10 -0800

    add favorites routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../../models/User');
+const Favorite = require('../../models/Favorite');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require("../../config/keys");
@@ -11,7 +12,9 @@ const validateLoginInput = require("../../validation/login");
 const sessions = require('./session');
 
 //User profile route
-router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get('/:id', 
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
     User
         .findById(req.params.id)
         .then(user => {
@@ -22,7 +25,7 @@ router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
         })
 })
 
-
+//do we need this?
 router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
   res.json({
       id: req.user.id,
@@ -30,7 +33,8 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
       email: req.user.email
   });
 });
-// signup controller
+
+// signup route
 router.post('/signup', (req, res) => {
     //deconstruct validation errors
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -66,47 +70,59 @@ router.post('/signup', (req, res) => {
         })
 })
 
-//login controller
+//login route
 router.post('/login', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const { errors, isValid } = validateLoginInput(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  const { errors, isValid } = validateLoginInput(req.body);
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-    User.findOne({email})
-        .then(user => {
-            if(!user) {
-                errors.email = "Can't find that surfer"
-                return res.status(404).json(errors);
+  User.findOne({email})
+    .then(user => {
+      if(!user) {
+        errors.email = "Can't find that surfer"
+        return res.status(404).json(errors);
+      }
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            const payload = {
+              id: user.id, 
+              handle: user.handle
+            };
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                //key expires in one hour
+                {expiresIn: 3600},
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                  });
+                }
+            );
+          } else {
+              return res.status(400).json({password: 'Incorrect password'})
             }
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if(isMatch) {
-                        const payload = {
-                            id: user.id, 
-                            handle: user.handle
-                        };
-
-                        jwt.sign(
-                            payload,
-                            keys.secretOrKey,
-                            //key expires in one hour
-                            {expiresIn: 3600},
-                            (err, token) => {
-                                res.json({
-                                    success: true,
-                                    token: 'Bearer ' + token
-                                });
-                            }
-                        );
-                    } else {
-                        return res.status(400).json({password: 'Incorrect password'})
-                    }
-                })
         })
+    })
 })
+
+// user's favorites route
+router.get('/:id/favorites', 
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Favorite
+      .find({ creatorId: req.params.id })
+      .sort({ createdAt: -1 })
+      .then(favorites => res.json(favorites))
+      .catch(err => res.status(400).json(err))
+    })
+
 router.use("/:id/sessions", sessions)
+
 module.exports = router;
