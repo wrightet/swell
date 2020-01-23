1fa1ced65e22b035855ce0bdb1e519484e9fcfa8
alscotty <alawrencescott@gmail.com>
2020-01-23 12:06:11 -0800

    add markers, one at a time to check

--- frontend/src/components/surfspots/surf_spot.js
+++ frontend/src/components/surfspots/surf_spot.js
@@ -5,31 +5,48 @@ class SurfSpot extends Component {
     constructor(props) {
         super(props);
         this.state={
-            checkpos:''
+            checkpos:'',
+            checkMarker:'',
+            gMap:''
         }
         
         this.useLatLng=this.useLatLng.bind(this);
     }
 
+    //change clicked lat and lng into usable variables, save temporarily in state
+    //place marker
     useLatLng(e){
         let lat= e.latLng['lat']();
         let lng = e.latLng['lng']();
         this.setState({checkpos: {lat:lat, lng:lng}})
+        
+        if (this.state.checkMarker){
+            this.state.checkMarker.setMap(null);
+        }
+
+        this.state.checkMarker=new window.google.maps.Marker({
+            position:this.state.checkpos,
+            map: this.state.gMap,
+            animation: window.google.maps.Animation.DROP
+        });
+
     }
 
     //goes to current location if allowed, otherwise centers on SF
     initMap() {
-        let gMap = new window.google.maps.Map(document.getElementById('add-spots-map'), {
+        this.state.gMap = new window.google.maps.Map(document.getElementById('add-spots-map'), {
             zoom: 13,
             maxZoom: 15
         });
 
-        navigator.geolocation.getCurrentPosition(function (position) {
+        const {gMap}=this.state;
+
+        navigator.geolocation.getCurrentPosition((position)=> {
             // Center on user's current location if geolocation prompt allowed
             var initialLocation = new window.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
             gMap.setCenter(initialLocation);
             gMap.setZoom(13);
-        }, function () {
+        }, ()=> {
             gMap.setCenter({ lat: 37.773972, lng: -122.431297 })
         })
 
@@ -51,8 +68,10 @@ class SurfSpot extends Component {
                 <br/>
                 <h3>Click on the map to checkout out spots</h3>
                 <br/>
-                Current spot: lat:{this.state.checkpos['lat']},
-                            lng:{this.state.checkpos['lng']}
+                Current spot: 
+                <br/>
+                    lat:{this.state.checkpos['lat']},
+                    lng:{this.state.checkpos['lng']}
                 <div id='add-spots-map'>
                 </div>
             </div>
