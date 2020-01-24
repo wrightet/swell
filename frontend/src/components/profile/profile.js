afa2496f86962f4db86423ed58017174b869e981
Martin Stenflo <stenflo@gmail.com>
2020-01-23 16:19:18 -0800

    update profile

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -6,9 +6,9 @@ class Profile extends React.Component {
   render() {
     return (
       <div className="Profile">
-        {this.props.currentUser.handle} &nbsp;
+        {/* {this.props.currentUser.handle} &nbsp;
         {this.props.currentUser.firstName} &nbsp;
-        {this.props.currentUser.lastName}
+        {this.props.currentUser.lastName} */}
       </div>
     );
   }
