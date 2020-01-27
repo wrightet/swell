b51eb46b69939a8deff55a15dbf281175b60a86f
Martin Stenflo <stenflo@gmail.com>
2020-01-26 18:31:35 -0800

    Surf session conflict

--- frontend/src/components/surf_session/super_surf_session.jsx
+++ frontend/src/components/surf_session/super_surf_session.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const SuperSurfSessionContainer = function(){
     return(
         <div>
+            <SurfSessionContainer/>
             <SurfSessionIndexContainer/>
         </div>
 
